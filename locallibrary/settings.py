899a0c6f3200ebad438f73e1781f8d174ca465f1
linzhaoqin <linzhaoqin@linzhaoqindeMacBook-Pro.local>
2018-01-07 21:11:24 +0800

    Added files and changes required for deployment to heroku

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -125,6 +125,8 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 
 
+
+# Heroku: Update database configuration from $DATABASE_URL
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
@@ -142,3 +144,4 @@ STATIC_URL = '/static/'
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
